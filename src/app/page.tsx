b64883cf813f22a3a5571cfa86cb8b43a915cedb
ekaadev <ekaadev9@gmail.com>
2025-08-12 03:20:58 +0700

    Refactor Home component layout and add social media links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,89 @@
+import ProjectList from "@/components/sections/project-list";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="container mx-auto md:px-44 px-8">
+      {/* Profile */}
+      <section
+        id="profile"
+        className="flex flex-col-reverse gap-4 py-[2rem] md:flex-row md:gap-8 md:py-[8rem] justify-between items-center"
+      >
+        {/* Left Side */}
+        <div className="gap-6 flex flex-col max-w-2xl">
+          <div className="flex flex-col gap-2">
+            <h1 className="md:text-5xl text-2xl">Yohanes Eka Putra</h1>
+            <h5 className="text-xl md:text-3xl font-medium">
+              Software Engineer
+            </h5>
+          </div>
+          <p className="text-sm font-medium text-gray-700">
+            I am a Software Engineer specializing in <b> Web Development</b>. My
+            passion also extends to <b>Game Development</b>. Feel free to
+            explore my projects to see my skills in action.
+          </p>
+          <Button className="w-fit">Let&apos;s work together</Button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {/* Social Links */}
+          <div className="flex gap-4 mb-8">
+            <a
+              href="https://www.instagram.com/_yohanesseka/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              <FaInstagram size={24} />
+            </a>
+            <a
+              href="https://github.com/ekaadev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/yohanes-eka-putra-85952128a/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-600 hover:text-gray-900"
+            >
+              <FaLinkedin size={24} />
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {/* Right Side */}
+        <div className="w-full flex justify-center">
+          <div className="relative w-32 h-32 md:w-72 md:h-72 max-w-full">
+            <Image
+              src="/notion-face-transparent.png"
+              alt="Profile Picture"
+              fill
+              className="rounded-full object-cover bg-white border-2 border-zinc-300"
+              sizes="(max-width: 768px) 128px, 288px"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Projects */}
+      <section id="projects">
+        <h1 className="md:text-2xl text-lg">
+          Here are some of my recent projects
+        </h1>
+        {/* List of Proejcts */}
+        <ProjectList />
+      </section>
+
+      {/* Footer */}
+      <footer className="pt-[2rem] md:pt-[8rem] pb-8 text-start">
+        <p className="text-sm text-zinc-500">
+          © {new Date().getFullYear()} Yohanes Eka Putra.
+        </p>
       </footer>
     </div>
   );
