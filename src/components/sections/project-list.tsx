65dbb30de73ead7f929a5ed0508a3214dbc80fbe
ekaadev <ekaadev9@gmail.com>
2025-08-12 03:39:42 +0700

    Fix typos in project descriptions and improve layout styling in ProjectList component

--- src/components/sections/project-list.tsx
+++ src/components/sections/project-list.tsx
@@ -17,28 +17,28 @@ const ProjectList = () => {
     {
       title: "Kaen",
       image: "/kaen-img.png", // Replace with actual image path
-      description: "An rouglike game made with Godot Engine",
+      description: "A rouglike game built with Godot Engine",
       technologies: ["Godot Engine", "Godot Script", "Aseprite"],
       link: "https://ek4ce.itch.io/kaen",
     },
     {
       title: "Clean Shoe",
       image: "/clean-shoe-img.png", // Replace with actual image path
-      description: "An service application for shoe cleaning",
+      description: "A service application for shoe cleaning",
       technologies: ["Svelte Kit", "Typescript", "Supabase", "Tailwind CSS"],
       link: "https://clean-shoe.vercel.app/",
     },
     {
       title: "MBTI Test",
       image: "/mbti-img.png", // Replace with actual image path
-      description: "An web application for MBTI personality test",
+      description: "A web application for MBTI personality test",
       technologies: ["Svelte Kit", "Typescript", "Tailwind CSS"],
       link: "https://ukdc-kbu-mbti-1.joelmedia.my.id/",
     },
     {
       title: "Giros",
       image: "/giros-img.png", // Replace with actual image path
-      description: "An application for rate your github profile",
+      description: "A application for rate your Github profile",
       technologies: ["Kotlin", "Android Studio"],
       link: "https://developerinformatikaukdc.itch.io/giros",
     },
@@ -46,25 +46,25 @@ const ProjectList = () => {
       title: "Eisenhower Todo API",
       image: "/github-img.webp", // Replace with actual image path
       description:
-        "An RESTful API for managing tasks using the Eisenhower Matrix",
+        "A RESTful API for managing tasks using the Eisenhower Matrix",
       technologies: ["Go", "Http Router", "PostgreSQL"],
       link: "https://github.com/ekaadev/eisenhower-todo-api",
     },
     {
       title: "My Pocket API",
       image: "/github-img.webp", // Replace with actual image path
-      description: "An RESTful API for my pocket application",
+      description: "A RESTful API for My Pocket application",
       technologies: ["Express JS", "Javascript"],
       link: "https://github.com/ekaadev/my-pocket-backend",
     },
   ];
 
   return (
-    <div className="flex flex-col items-center gap-4 md:flex-row md:flex-wrap md:items-stretch">
+    <div className="flex flex-col gap-4 md:flex-row md:flex-wrap">
       {projects.map((project, index) => (
         <Card
           key={index}
-          className="my-2 w-full max-w-xs sm:max-w-sm flex flex-col flex-1"
+          className="my-4 w-full max-w-xs sm:max-w-sm md:max-w-sm md:flex-none"
         >
           <CardContent>
             <div className="aspect-[16/9] w-full mb-4 rounded-lg overflow-hidden relative border">
