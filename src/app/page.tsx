81438116a7bf9efad60137f6f94388b4ff4745df
ekaadev <ekaadev9@gmail.com>
2025-08-19 01:38:32 +0700

    Refactor Home component to improve layout and add navigation links; integrate About, Education, and Skills sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,140 +1,132 @@
 "use client";
 
-import ProjectList from "@/components/sections/project-list";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
+
+import { CgProfile } from "react-icons/cg";
+import { FaBookOpen } from "react-icons/fa";
+import { IoCodeOutline } from "react-icons/io5";
 import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
-import { useEffect, useState } from "react";
+
+import EducationList from "@/components/sections/education-list";
+import ProjectList from "@/components/sections/project-list";
+import SkillList from "@/components/sections/skill-list";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setIsLoading(false), 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="container mx-auto md:px-44 px-8">
-        <section className="flex flex-col-reverse gap-4 py-[2rem] md:flex-row md:gap-8 md:py-[8rem] justify-between items-center">
-          <div className="gap-6 flex flex-col max-w-2xl w-full">
-            <Skeleton className="h-12 w-2/3 mb-2" />
-            <Skeleton className="h-8 w-1/2 mb-4" />
-            <Skeleton className="h-5 w-full mb-2" />
-            <Skeleton className="h-5 w-5/6 mb-2" />
-            <Skeleton className="h-10 w-40 mb-6" />
-            <div className="flex gap-4 mb-8">
-              <Skeleton className="h-8 w-8 rounded-full" />
-              <Skeleton className="h-8 w-8 rounded-full" />
-              <Skeleton className="h-8 w-8 rounded-full" />
-            </div>
-          </div>
-          <div className="w-full flex justify-center">
-            <Skeleton className="rounded-full w-32 h-32 md:w-72 md:h-72" />
-          </div>
-        </section>
-        <section id="projects">
-          <Skeleton className="h-8 w-1/2 mb-4" />
-          <div className="flex flex-col md:flex-row md:flex-wrap gap-4">
-            {[...Array(3)].map((_, i) => (
-              <Skeleton
-                key={i}
-                className="h-64 w-full max-w-xs sm:max-w-sm rounded-lg"
-              />
-            ))}
-          </div>
-        </section>
-        <footer className="pt-[2rem] md:pt-[8rem] pb-8 text-start">
-          <Skeleton className="h-4 w-1/3" />
-        </footer>
-      </div>
-    );
-  }
-
   return (
-    <div className="container mx-auto md:px-44 px-8">
-      {/* Profile */}
-      <section
-        id="profile"
-        className="flex flex-col-reverse gap-4 py-[2rem] md:flex-row md:gap-8 md:py-[8rem] justify-between items-center"
-      >
-        {/* Left Side */}
-        <div className="gap-6 flex flex-col max-w-2xl">
-          <div className="flex flex-col gap-2">
-            <h1 className="md:text-5xl text-2xl">Yohanes Eka Putra</h1>
-            <h5 className="text-xl md:text-3xl font-medium">
-              Software Engineer
-            </h5>
-          </div>
-          <p className="text-sm md:text-lg font-medium text-gray-700">
-            I am a Software Engineer specializing in <b> Web Development</b>. My
-            passion also extends to <b>Game Development</b>. Feel free to
-            explore my projects to see my skills in action.
-          </p>
+    <main className="max-w-2xl mx-auto px-4">
+      {/* Navigation */}
+      <nav className="fixed top-1/2 right-0 -translate-y-1/2 -translate-x-1/2 border max-w-[3rem] px-6 rounded-xl bg-gray-100">
+        <div className="flex flex-col items-center space-y-0.5 py-2">
+          <a
+            href="#profile"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
+          >
+            <CgProfile className="text-2xl text-gray-500" />
+          </a>
+          <a
+            href="#education"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
+          >
+            <FaBookOpen className="text-2xl text-gray-500" />
+          </a>
+          <a
+            href="#projects"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
+          >
+            <IoCodeOutline className="text-2xl text-gray-500" />
+          </a>
+        </div>
+        <div className="flex flex-col items-center space-y-0.5 py-2">
+          <a
+            href="https://www.instagram.com/_yohanesseka/"
+            target="_blank"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
+          >
+            <FaInstagram className="text-2xl text-gray-500" />
+          </a>
           <a
             href="https://www.linkedin.com/in/yohanes-eka-putra-85952128a/"
             target="_blank"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
           >
-            <Button className="w-fit">Let&apos;s work together</Button>
+            <FaLinkedin className="text-2xl text-gray-500" />
           </a>
+          <a
+            href="https://github.com/ekaadev"
+            target="_blank"
+            className="p-2 hover:bg-gray-300 hover:rounded-full transition-all duration-150 ease-in-out"
+          >
+            <FaGithub className="text-2xl text-gray-500" />
+          </a>
+        </div>
+      </nav>
 
-          {/* Social Links */}
-          <div className="flex gap-4 mb-8">
-            <a
-              href="https://www.instagram.com/_yohanesseka/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-900"
-            >
-              <FaInstagram size={24} />
-            </a>
-            <a
-              href="https://github.com/ekaadev"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-900"
-            >
-              <FaGithub size={24} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/yohanes-eka-putra-85952128a/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-900"
-            >
-              <FaLinkedin size={24} />
-            </a>
+      {/* Profile */}
+      <section
+        id="profile"
+        className="flex flex-row pt-[2rem] md:pt-[4rem] justify-between items-center"
+      >
+        {/* Left Side */}
+        <div className="gap-6 flex flex-col w-full flex-2">
+          <div className="flex flex-col gap-2">
+            <h1 className="md:text-4xl text-2xl">Hi, I&apos;m Eka</h1>
+            <h5 className="text-sm md:text-lg font-medium">
+              Computer Science Student, Software Engineer
+            </h5>
           </div>
         </div>
         {/* Right Side */}
-        <div className="w-full flex justify-center">
-          <div className="relative w-32 h-32 md:w-72 md:h-72 max-w-full">
+        <div className="w-full flex justify-end flex-1">
+          <div className="relative w-12 h-12 md:w-36 md:h-36 max-w-full">
             <Image
-              src="/notion-face-transparent.png"
+              src="/profile-github.png"
               alt="Profile Picture"
               fill
-              className="rounded-full object-cover bg-white border-2 border-zinc-300"
+              className="rounded-full object-cover border border-zinc-300"
               sizes="(max-width: 768px) 128px, 288px"
             />
           </div>
         </div>
       </section>
 
+      {/* About */}
+      <section id="about" className="mt-4">
+        <h2 className="text-xl">About</h2>
+        <hr className="items-center my-1" />
+        <p className="text-sm font-normal text-gray-700">
+          I am a Software Engineer specializing in <b> Web Development</b>. My
+          passion also extends to <b>Game Development</b>. Feel free to explore
+          my projects to see my skills in action.
+        </p>
+      </section>
+
+      {/* Education */}
+      <section id="education" className="mt-12">
+        <h2 className="text-xl">Education</h2>
+        <hr className="items-center mb-2 mt-1" />
+        <EducationList />
+      </section>
+
       {/* Projects */}
-      <section id="projects">
-        <h1 className="md:text-2xl text-lg">My Recent Projects</h1>
-        {/* List of Proejcts */}
+      <section id="projects" className="mt-12">
+        <h2 className="text-xl">Projects</h2>
+        <hr className="items-center mb-2 mt-1" />
         <ProjectList />
       </section>
 
+      {/* Skills */}
+      <section id="projects" className="mt-12">
+        <h2 className="text-xl">Skills</h2>
+        <hr className="items-center mb-2 mt-1" />
+        <SkillList />
+      </section>
+
       {/* Footer */}
-      <footer className="pt-[2rem] md:pt-[8rem] pb-8 text-start">
+      <footer className="pt-[2rem] md:pt-[4rem] pb-8 text-start">
         <p className="text-sm text-zinc-500">
           © {new Date().getFullYear()} Yohanes Eka Putra.
         </p>
       </footer>
-    </div>
+    </main>
   );
 }
